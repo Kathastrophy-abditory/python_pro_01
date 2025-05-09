1c7f4f54771c132a7197a6222eaf2869736f28cb
KARA <katea.riveraa@gmail.com>
2025-05-08 19:29:13 -0600

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def en_lines():
+    print(f'Tu bot {bot.user} esta en linea')
+
+@bot.command()
+async def saludar(ctx,*,mensaje:str):
+    
+    mensaje = mensaje.lower().strip()
+    
+    if mensaje == 'hola':
+        await ctx.send('Hola, ¿cómo estás?')
+    elif mensaje == 'klk':
+        await ctx.send('Klk, ¿todo bien?')
+    elif mensaje == 'buenas':
+        await ctx.send('Buenas, ¿qué tal?')
+    else:
+        await ctx.send('SALUDA BIEN!!!')
+
+@bot.command()
+async def suma(ctx, left: int, right: int):
+    await ctx.send(f'Result: {left + right}')
+
+token = ""
+
+bot.run(token)
